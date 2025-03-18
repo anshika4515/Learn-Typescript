5bf059aa55523fe35b15dadc2c373a7a2883db2a
anshika4515 <anshikaverma4515@gmail.com>
2025-03-18 19:03:41 +0530

    18 March | abt tuples , abt enums

---
+++ 02BASICS/myTupEnums.js
@@ -0,0 +1,3 @@
+//Tuple is all about restricting us so that we put data in proper order
+var tUser;
+tUser = ["anshika", 989, false];
---
+++ 02BASICS/myTupEnums.ts
@@ -0,0 +1,5 @@
+//Tuple is all about restricting us so that we put data in proper order
+
+let tUser: [string, number, boolean]
+tUser = ["anshika",989,false]
+
